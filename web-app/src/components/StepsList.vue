3426231c72bdb88e93f242976ccdca965e3c381b
Akash <ashah10@sapient.com>
2018-12-24 10:27:32 +0530

    updated prayer section

--- web-app/src/components/StepsList.vue
+++ web-app/src/components/StepsList.vue
@@ -3,7 +3,7 @@
     <v-ons-list-item
       modifier="longdivider"
       v-for="(step, idx) in steps"
-      v-bind="idx"
+      :bind="idx"
     >
       <span><i class="fa fa-circle-o"></i> &nbsp;{{ step }}</span>
     </v-ons-list-item>
--- web-app/src/pages/Prayer.vue
+++ web-app/src/pages/Prayer.vue
@@ -4,7 +4,32 @@
       <div class="left"><v-ons-back-button>Home</v-ons-back-button></div>
       <div class="center">PRAYER</div>
     </v-ons-toolbar>
-    <p style="text-align: center">Learn how to pray here...</p>
+    <CardWithFloralBorder>
+      <!-- <StepsList :steps="prayerSteps" /> -->
+      <v-ons-list>
+        <v-ons-list-item
+          v-for="(step, idx) in prayerSteps"
+          style="padding: 0px;"
+        >
+          <template v-if="typeof step === 'string'">
+            {{ step }}
+          </template>
+
+          <template v-else>
+            <div style="text-align:center; margin:0px;">
+              <p
+                v-for="(prayer, idx) in step.prayer"
+                :bind="idx"
+                style="display:inline-block; margin:2px 0;"
+              >
+                <i>{{ prayer }}</i
+                ><br />
+              </p>
+            </div>
+          </template>
+        </v-ons-list-item>
+      </v-ons-list>
+    </CardWithFloralBorder>
   </v-ons-page>
 </template>
 
@@ -13,7 +38,16 @@
 </style>
 
 <script>
+import CardWithFloralBorder from "../components/CardWithFloralBorder";
+import StepsList from "../components/StepsList";
+import prayerSteps from "../data/prayer";
 export default {
-  key: "PrayerPage"
+  key: "PrayerPage",
+  components: { CardWithFloralBorder, StepsList },
+  data() {
+    return {
+      prayerSteps
+    };
+  }
 };
 </script>
