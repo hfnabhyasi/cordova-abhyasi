fa0b8f966ddf45180e2df683da33adea066f36fd
Akash <ashah10@sapient.com>
2019-01-01 16:57:57 +0530

    restructuring

--- web-app-2/src/pages/home.vue
+++ web-app-2/src/pages/home.vue
@@ -1,10 +1,29 @@
 <template>
   <f7-page name="home">
-    <f7-navbar title="Home"></f7-navbar>
+    <f7-navbar title="Abhyasi App"> </f7-navbar>
     <!-- Page content -->
-    <f7-link href="/about/">About Page</f7-link>
+    <f7-list>
+      <f7-list-item> <f7-link href="/about/">10 Maxims</f7-link> </f7-list-item>
+      <f7-list-item accordion-item title="Practice">
+        <f7-accordion-content>
+          <f7-list>
+            <f7-list-item>
+              <f7-link href="/login/">Relaxation</f7-link>
+            </f7-list-item>
+            <f7-list-item>
+              <f7-link href="/login/">Meditation</f7-link>
+            </f7-list-item>
+            <f7-list-item>
+              <f7-link href="/login/" animate="false">Cleaning</f7-link>
+            </f7-list-item>
+            <f7-list-item>
+              <f7-link href="/login/">Prayer</f7-link>
+            </f7-list-item>
+          </f7-list>
+        </f7-accordion-content>
+      </f7-list-item>
+    </f7-list>
     <br />
-    <f7-link href="/login/">Login Page</f7-link>
   </f7-page>
 </template>
 <script>
--- web-app-2/src/pages/login.vue
+++ web-app-2/src/pages/login.vue
@@ -1,6 +1,13 @@
 <template>
   <f7-page name="login">
-    <f7-navbar title="Login"></f7-navbar>
+    <!-- <f7-navbar title="Login" back-link="Back"></f7-navbar> -->
+    <f7-navbar>
+      <f7-nav-left back-link="Back"></f7-nav-left>
+      <f7-nav-title>Login Page</f7-nav-title>
+      <f7-nav-right>
+        <f7-link icon="icon-bars" panel-open="right"></f7-link>
+      </f7-nav-right>
+    </f7-navbar>
     <!-- Page content -->
     <f7-link href="/">Back Home</f7-link>
   </f7-page>
