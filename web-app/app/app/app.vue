1bd1d8f9df3b89fc29dbe43b6528e8ff17970b47
Akash <ashah10@sapient.com>
2019-01-01 14:42:33 +0530

    working mutli-page with cordova android build working condition

--- web-app/app/app/app.vue
+++ web-app/app/app/app.vue
@@ -1,35 +1,45 @@
+<!-- app.vue -->
 <template>
-  <!-- Main Framework7 App component where we pass Framework7 params -->
-  <f7-app :params="$root.f7params">
-    <f7-statusbar></f7-statusbar>
-    <f7-view main>
-      <!-- Initial Page -->
-      <f7-page>
-        <f7-navbar title="Awesome App"></f7-navbar>
-        <f7-toolbar>
-          <f7-link>Link 1</f7-link>
-          <f7-link>Link 2</f7-link>
-        </f7-toolbar>
-        <p>Page content goes here</p>
-        <f7-link href="/about/">About App</f7-link>
-      </f7-page>
-    </f7-view>
+  <f7-app :params="f7params">
+    <!-- Current View/Router, initial page will be loaded from home.vue component -->
+    <f7-view main url="/"></f7-view>
   </f7-app>
 </template>
+
 <script>
-// import routes from "./routes.js";
+/* Import pages components */
+import HomePage from "./home.vue";
+import AboutPage from "./about.vue";
+import LoginPage from "./login.vue";
+
+/*
+  Now we need to map components to routes.
+  We need to pass them along with the F7 app parameters to <f7-app> component
+  */
 
 export default {
   data() {
     return {
-      // Framework7 parameters that we pass to <f7-app> component
+      // app params
       f7params: {
-        // Array with app routes
-        // routes,
-        // App Name
         name: "My App",
-        // App id
-        id: "com.myapp.test"
+        id: "com.myapp.test",
+        // specify routes for app
+        routes: [
+          {
+            path: "/",
+            component: HomePage
+          },
+          {
+            path: "/about/",
+            component: AboutPage
+          },
+          {
+            path: "/login/",
+            component: LoginPage
+          }
+          // ...
+        ]
         // ...
       }
     };
