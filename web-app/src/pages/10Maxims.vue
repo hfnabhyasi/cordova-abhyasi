d0597a457c05aac16fc73c4c64943d871b990d7c
Akash <ashah10@sapient.com>
2019-01-12 08:53:18 +0530

    WIP

--- web-app/src/pages/10Maxims.vue
+++ web-app/src/pages/10Maxims.vue
@@ -1,11 +1,13 @@
 <template>
   <f7-page name="login">
     <f7-navbar title="10 Maxims" back-link="Back"> </f7-navbar>
-    <!-- <f7-navbar title="Login"></f7-navbar> -->
-    <!-- Page content -->
+
     <f7-card>
       <f7-list>
-        <!-- <f7-list-item @click="$f7router.back()" title="Back"></f7-list-item> -->
+        <f7-list> <f7-list-item title="Maxim 1"></f7-list-item> </f7-list>
+        <f7-list> <f7-list-item title="Maxim 2"></f7-list-item> </f7-list>
+        <f7-list> <f7-list-item title="Maxim 3"></f7-list-item> </f7-list>
+        <f7-list> <f7-list-item title="Maxim 4"></f7-list-item> </f7-list>
       </f7-list>
     </f7-card>
   </f7-page>
