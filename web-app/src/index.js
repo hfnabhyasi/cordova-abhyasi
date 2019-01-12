a25cc6a4bbc04356e16e046b7de19079b40c6d3c
Akash <ashah10@sapient.com>
2019-01-12 12:43:10 +0530

    updated package.json

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -1,9 +1,10 @@
 import "framework7/css/framework7.min.css";
-import '@fortawesome/fontawesome-free/css/all.css'
 import 'framework7-icons/css/framework7-icons.css'
+//
 import Vue from 'vue'
 import Framework7 from 'framework7/framework7.esm.bundle.js'
 import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js'
+//
 import App from './App.vue';
 
 Framework7.use(Framework7Vue);
