1335647ec6ab4d7aabaa607c94ab8f6f44f51a1a
Akash <ashah10@sapient.com>
2019-01-12 07:23:55 +0530

    created pages

---
+++ web-app/src/pages/Cleaning.vue
@@ -0,0 +1,15 @@
+<template>
+  <f7-page name="login">
+    <f7-navbar title="10 Maxims" back-link="Back"> </f7-navbar>
+    <!-- <f7-navbar title="Login"></f7-navbar> -->
+    <!-- Page content -->
+    <f7-card>
+      <f7-list>
+        <!-- <f7-list-item @click="$f7router.back()" title="Back"></f7-list-item> -->
+      </f7-list>
+    </f7-card>
+  </f7-page>
+</template>
+<script>
+export default {};
+</script>
