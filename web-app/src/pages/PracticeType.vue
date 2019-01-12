59df02921b0c31bcfe12d77bd8db705e1b55eb90
Akash <ashah10@sapient.com>
2019-01-12 11:11:25 +0530

    getting data from Practice.json

--- web-app/src/pages/PracticeType.vue
+++ web-app/src/pages/PracticeType.vue
@@ -1,11 +1,34 @@
 <template>
   <f7-page name="practice">
-    <f7-navbar :title="practice" back-link="Back"> </f7-navbar>
-    <f7-card> {{ practice }} </f7-card>
+    <f7-navbar class="capitalize" :title="practice" back-link="Back">
+    </f7-navbar>
+    <f7-card>
+      <f7-block>
+        <h1 class="capitalize">{{ practice }}</h1>
+        <ul>
+          <li v-for="step in Practice[practice]">
+            <p>{{ step }}</p>
+          </li>
+        </ul>
+      </f7-block>
+    </f7-card>
   </f7-page>
 </template>
 <script>
+import Practice from "../data/Practice";
 export default {
+  data() {
+    return { Practice };
+  },
   props: ["practice"]
 };
 </script>
+<style scoped>
+.capitalize {
+  text-transform: capitalize;
+}
+ul {
+  padding-bottom: 20px;
+  padding-left: 15px;
+}
+</style>
