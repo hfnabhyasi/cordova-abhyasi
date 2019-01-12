cc4c33b2144fe9f0868cea2c8eef3341dd977f5e
Akash <ashah10@sapient.com>
2019-01-12 07:41:34 +0530

    abstracting out routes

--- web-app/src/App.vue
+++ web-app/src/App.vue
@@ -7,60 +7,15 @@
 </template>
 
 <script>
-/* Import pages components */
-import HomePage from "./pages/Home.vue";
-import MaximsPage from "./pages/10Maxims.vue";
-import RelaxationPage from "./pages/Relaxation.vue";
-import MeditationPage from "./pages/Meditation.vue";
-import CleaningPage from "./pages/Cleaning.vue";
-import PrayerPage from "./pages/Prayer.vue";
-import AEIOUPage from "./pages/AEIOU.vue";
-
-/*
-  Now we need to map components to routes.
-  We need to pass them along with the F7 app parameters to <f7-app> component
-  */
+import routes from "./routes";
 
 export default {
   data() {
     return {
-      // app params
       f7params: {
         name: "My App",
         id: "com.myapp.test",
-        // specify routes for app
-        routes: [
-          {
-            path: "/",
-            component: HomePage
-          },
-          {
-            path: "/maxims/",
-            component: MaximsPage
-          },
-          {
-            path: "/relaxation/",
-            component: RelaxationPage
-          },
-          {
-            path: "/meditation/",
-            component: MeditationPage
-          },
-          {
-            path: "/cleaning/",
-            component: CleaningPage
-          },
-          {
-            path: "/prayer/",
-            component: PrayerPage
-          },
-          {
-            path: "/aeiou/",
-            component: AEIOUPage
-          }
-          // ...
-        ]
-        // ...
+        routes
       }
     };
   }
---
+++ web-app/src/routes/index.js
@@ -0,0 +1,39 @@
+
+import HomePage from "../pages/Home.vue";
+import MaximsPage from "../pages/10Maxims.vue";
+import RelaxationPage from "../pages/Relaxation.vue";
+import MeditationPage from "../pages/Meditation.vue";
+import CleaningPage from "../pages/Cleaning.vue";
+import PrayerPage from "../pages/Prayer.vue";
+import AEIOUPage from "../pages/AEIOU.vue";
+
+export default [
+  {
+    path: "/",
+    component: HomePage
+  },
+  {
+    path: "/maxims/",
+    component: MaximsPage
+  },
+  {
+    path: "/relaxation/",
+    component: RelaxationPage
+  },
+  {
+    path: "/meditation/",
+    component: MeditationPage
+  },
+  {
+    path: "/cleaning/",
+    component: CleaningPage
+  },
+  {
+    path: "/prayer/",
+    component: PrayerPage
+  },
+  {
+    path: "/aeiou/",
+    component: AEIOUPage
+  }
+]
