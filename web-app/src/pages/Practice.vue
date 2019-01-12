c07275489055a0de09d10605254fd36dd3748126
Akash <ashah10@sapient.com>
2019-01-12 10:57:37 +0530

    WIP

--- web-app/src/pages/Practice.vue
+++ web-app/src/pages/Practice.vue
@@ -4,7 +4,7 @@
     <f7-card>
       <f7-list>
         <f7-list-item
-          v-for="item in listItems"
+          v-for="item in PracticePageListItems"
           :title="item.title"
           :link="item.link"
           no-chevron
@@ -14,27 +14,11 @@
   </f7-page>
 </template>
 <script>
+import PracticePageListItems from "../data/PracticePageListItems";
 export default {
   data() {
     return {
-      listItems: {
-        relaxation: {
-          title: "Relaxation",
-          link: "/practice/relaxation"
-        },
-        meditation: {
-          title: "Meditation",
-          link: "/practice/meditation"
-        },
-        cleaning: {
-          title: "Cleaning",
-          link: "/practice/cleaning"
-        },
-        prayer: {
-          title: "Prayer",
-          link: "/practice/prayer"
-        }
-      }
+      PracticePageListItems
     };
   }
 };
