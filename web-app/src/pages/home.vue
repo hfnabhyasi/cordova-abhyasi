1f070e49a908d34cbf5fed9520a0e30401dcdfe3
Akash <ashah10@sapient.com>
2019-01-12 07:16:29 +0530

    back button

--- web-app/src/pages/home.vue
+++ web-app/src/pages/home.vue
@@ -2,21 +2,43 @@
   <f7-page name="home">
     <f7-navbar title="Home"></f7-navbar>
     <!-- Page content -->
-    <f7-list accordion-list>
-      <f7-list-item title="10 Maxims" link="/about/"></f7-list-item>
-      <f7-list-item accordion-item title="Abhyaas / Practice">
-        <f7-accordion-content>
-          <f7-block>
-            <f7-list>
-              <f7-list-item title="Relaxation" link="/login/"></f7-list-item>
-              <f7-list-item title="Meditation" link="/login/"></f7-list-item>
-              <f7-list-item title="Cleaning" link="/login/"></f7-list-item>
-              <f7-list-item title="Prayer" link="/login/"></f7-list-item>
-            </f7-list>
-          </f7-block>
-        </f7-accordion-content>
-      </f7-list-item>
-    </f7-list>
+    <f7-card>
+      <f7-list accordion-list>
+        <f7-list-item
+          title="10 Maxims"
+          link="/about/"
+          no-chevron
+        ></f7-list-item>
+        <f7-list-item accordion-item title="Abhyaas / Practice">
+          <f7-accordion-content>
+            <f7-block>
+              <f7-list>
+                <f7-list-item
+                  title="Relaxation"
+                  link="/about/"
+                  no-chevron
+                ></f7-list-item>
+                <f7-list-item
+                  title="Meditation"
+                  link="/login/"
+                  no-chevron
+                ></f7-list-item>
+                <f7-list-item
+                  title="Cleaning"
+                  link="/about/"
+                  no-chevron
+                ></f7-list-item>
+                <f7-list-item
+                  title="Prayer"
+                  link="/login/"
+                  no-chevron
+                ></f7-list-item>
+              </f7-list>
+            </f7-block>
+          </f7-accordion-content>
+        </f7-list-item>
+      </f7-list>
+    </f7-card>
   </f7-page>
 </template>
 <script>
